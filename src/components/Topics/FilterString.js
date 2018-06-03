85d1b7fad7ffc4af09deeb877accdfe1b27a7f9c
Goyo <gregory.vargas18@gmail.com>
2018-06-03 13:04:52 -0500

    completed step 5

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
 
 class FilterString extends Component {
+  constructor() {
+    super();
+    this.state = {
+      unFilteredArray : ["James", "Jessica", "Melody", "Tyler", "Blake", "Jennifer", "Mark", "Maddy"],
+      userInput : "",
+      filteredArray : []
+    }
+  }
+
+  newInput(str) {
+    this.setState( { userInput : str } )
+  }
+
+  filterArr(str) {
+    let filteredArr = [];
+    let counter = 0;
+    // iterates through unFilteredArray
+    for (let i = 0; i < this.state.unFilteredArray.length; i++) {
+      counter = 0;
+      // iterates through each individual letter in str input
+      for (let j = 0; j < str.length; j++) {
+        if (this.state.unFilteredArray[i][j] === str[j]) {
+          counter += 1;
+          if (counter === str.length) {
+            filteredArr.push(this.state.unFilteredArray[i]);
+          }
+        }
+      }
+    }
+    this.setState( { filteredArray : filteredArr } );
+  }
+
   render() {
     return (
-      <p>FilterString Component</p>
+      <div className="puzzleBox filterStringPB">
+        <h4>Filter String</h4>
+        <span className="puzzleText">Original: { JSON.stringify(this.state.unFilteredArray) }</span>
+        <input className="inputLine" onChange={ (event) => { this.newInput(event.target.value) } }></input>
+        <button className="confirmationButton" onClick={ () => { this.filterArr(this.state.userInput) } }>FILTER</button>
+        <span className="resultsBox filterStringRB">Filtered: { JSON.stringify(this.state.filteredArray) }</span>
+      </div>
     );
   }
 }
